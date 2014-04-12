cf7b964c27fe8b7b5ff647bdd13bfc4ffe827e61
Haochen Wu <wuhaochen1990@gmail.com>
2014-04-12 11:38:02 -0400

    Add HeroNavigationViewController and collection view and cell view controller and reusable view controller

---
+++ DictOfDota2/HeroNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  HeroNavigationViewController.h
+//  DictOfDota2
+//
+//  Created by Haochen Wu on 4/12/14.
+//  Copyright (c) 2014 Haochen Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeroNavigationViewController : UIViewController
+
+@end
