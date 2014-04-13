8e46db6078800ec97033e8a5e030616415101708
Haochen Wu <wuhaochen1990@gmail.com>
2014-04-13 17:14:27 -0400

    add hero detail to chaos knight

--- DictOfDota2/HeroInfoViewController.m
+++ DictOfDota2/HeroInfoViewController.m
@@ -71,13 +71,13 @@ AVAudioPlayer *heroAudio;
     //set the heroDetail
     detailDict = @{
                    @"Abaddon": [NSString stringWithFormat:@"Abaddon源于圣经启示录代表地狱的恶魔"],
-                   @"Alchemist": [NSString stringWithFormat:@"炼金术士"],
-                   @"Axe": [NSString stringWithFormat:@"斧王"],
-                   @"Beastmaster": [NSString stringWithFormat:@"兽王"],
-                   @"Brewmaster": [NSString stringWithFormat:@"熊猫酒仙"],
-                   @"Bristleback": [NSString stringWithFormat:@"刚背兽"],
-                   @"Centaur": [NSString stringWithFormat:@"半人马酋长"],
-                   @"Chaos_Knight": [NSString stringWithFormat:@"混沌骑士"],
+                   @"Alchemist": [NSString stringWithFormat:@"Alchemy－炼金术\nAlchemist－炼金师"],
+                   @"Axe": [NSString stringWithFormat:@"axe－斧头"],
+                   @"Beastmaster": [NSString stringWithFormat:@"beast－野兽\nmaster－大师"],
+                   @"Brewmaster": [NSString stringWithFormat:@"brew－酿酒\nmaster－大师"],
+                   @"Bristleback": [NSString stringWithFormat:@"bristle－刚毛\nback－后背"],
+                   @"Centaur": [NSString stringWithFormat:@"centaur源于希腊神话中半人半马的生物"],
+                   @"Chaos_Knight": [NSString stringWithFormat:@"chaos－混乱\nknight－骑士"],
                         };
     [[self HeroDetail]setText:detailDict[self.heroName]];
     
