145b99e5f6f4ad2e40a14d6c8fbe4c3fd015f7a9
Haochen Wu <wuhaochen1990@gmail.com>
2014-05-03 16:01:50 -0400

    complete image of items

--- DictOfDota2/ItemNavigationViewController.m
+++ DictOfDota2/ItemNavigationViewController.m
@@ -35,9 +35,139 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     //set the 3 item array
-    basicsItem = [[NSArray alloc] initWithObjects: @"clarity", nil];
-    
-    
+    basicsItem = [[NSArray alloc] initWithObjects:
+                  @"belt of strength",
+                  @"blade of alacrity",
+                  @"blades of attack",
+                  @"blink dagger",
+                  @"boots of elves",
+                  @"boots",
+                  @"bottle",
+                  @"branches",
+                  @"broadsword",
+                  @"chainmail",
+                  @"circlet",
+                  @"clarity",
+                  @"claymore",
+                  @"cloak",
+                  @"courier",
+                  @"dust",
+                  @"flask",
+                  @"flying courier",
+                  @"gauntlets",
+                  @"gem",
+                  @"ghost",
+                  @"gloves",
+                  @"helm of iron vwill",
+                  @"javelin",
+                  @"lifesteal",
+                  @"magic_stick",
+                  @"mantle",
+                  @"mithril hammer",
+                  @"observer ward",
+                  @"ogre axe",
+                  @"platemail",
+                  @"quarterstaff",
+                  @"quelling blade",
+                  @"ring of protection",
+                  @"ring of regen",
+                  @"robe",
+                  @"sentry ward",
+                  @"shadow amulet",
+                  @"slippers",
+                  @"smoke of deceit",
+                  @"sobi mask",
+                  @"staff of wizardry",
+                  @"stout shield",
+                  @"talisman of evasion",
+                  @"tango",
+                  @"tpscroll",
+                  @"ultimate orb",nil];
+    upgradesItem = [[NSArray alloc] initWithObjects:
+                    @"abyssal blade",
+                    @"aghanim's scepter",
+                    @"arcane boots",
+                    @"armlet of mordiggian",
+                    @"assault cuirass",
+                    @"battle fury",
+                    @"black king bar",
+                    @"blade mail",
+                    @"bloodstone",
+                    @"bracer",
+                    @"buckler",
+                    @"butterfly",
+                    @"crystalys",
+                    @"daedalus",
+                    @"dagon",
+                    @"desolator",
+                    @"diffusal blade",
+                    @"divine rapier",
+                    @"drum of endurance",
+                    @"ethereal blade",
+                    @"eul's scepter of divinity",
+                    @"eye of Skadi",
+                    @"force staff",
+                    @"hand of midas",
+                    @"headdress",
+                    @"heart of Tarrasque",
+                    @"heaven's halberd",
+                    @"helm of the dominator",
+                    @"hood of defiance",
+                    @"Linken's sphere",
+                    @"maelstrom",
+                    @"magic wand",
+                    @"manta style",
+                    @"mask of madness",
+                    @"medallion of courage",
+                    @"mekansm",
+                    @"mjollnir",
+                    @"monkey king bar",
+                    @"necronomicon",
+                    @"null talisman",
+                    @"oblivion staff",
+                    @"orchid malevolence",
+                    @"perseverance",
+                    @"phase boots",
+                    @"pipe of insight",
+                    @"poor mans shield",
+                    @"power treads",
+                    @"radiance",
+                    @"refresher orb",
+                    @"ring of aquila",
+                    @"ring of basilius",
+                    @"rod of atos",
+                    @"sange and yasha",
+                    @"sange",
+                    @"satanic",
+                    @"scythe of vyse",
+                    @"shadow blade",
+                    @"shiva's guard",
+                    @"skull basher",
+                    @"soul booster",
+                    @"soul ring",
+                    @"tranquil boots",
+                    @"travel boots",
+                    @"urn of shadows",
+                    @"vanguard",
+                    @"veil of discord",
+                    @"vladmir's offering",
+                    @"wraith band",
+                    @"yasha",nil];
+    secretItem = [[NSArray alloc] initWithObjects:
+                  @"demon edge",
+                  @"eaglesong",
+                  @"energy booster",
+                  @"hyperstone",
+                  @"mystic staff",
+                  @"orb of venom",
+                  @"point booster",
+                  @"reaver",
+                  @"ring of health",
+                  @"sacred relic",
+                  @"vitality booster",
+                  @"void stone",nil];
+
+
     
 }
 
@@ -84,8 +214,16 @@
             [item.itemImage setImage:[UIImage imageNamed:itemImage]];
             break;
         case 1:
+            itemName = [upgradesItem objectAtIndex:indexPath.item];
+            itemImage = [itemName stringByAppendingString:@".png"];
+            [item.itemName setText:itemName];
+            [item.itemImage setImage:[UIImage imageNamed:itemImage]];
             break;
         case 2:
+            itemName = [secretItem objectAtIndex:indexPath.item];
+            itemImage = [itemName stringByAppendingString:@".png"];
+            [item.itemName setText:itemName];
+            [item.itemImage setImage:[UIImage imageNamed:itemImage]];
             break;
         default:
             break;
